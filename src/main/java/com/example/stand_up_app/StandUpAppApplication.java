0205db5db1540531c75c8a01e873701b11ae9734
LSStefan <stefanlican@gmail.com>
2026-01-04 18:19:39 +0200

    initial

---
+++ src/main/java/com/example/stand_up_app/StandUpAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stand_up_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandUpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandUpAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stand_up_app/StandUpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stand_up_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandUpAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
